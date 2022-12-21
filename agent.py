5d2da806678f157cba1fe6d5ec3c465db9cfffe0
sonnguyen112 <nguyenhotruongson1234@gmail.com>
2022-12-21 17:55:09 +0700

    commit

--- agent.py
+++ agent.py
@@ -1,5 +1,3 @@
-import random
-
 class Agent():
     def __init__(self):
         self.__is_win = False
--- map.py
+++ map.py
@@ -1,2 +1,6 @@
 class Map():
-    pass
+    def __init__(self, map_file):
+        self.map = self.read_map(map_file)
+
+    def read_map(self):
+        pass
