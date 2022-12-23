c6dfad864c15b362e894dd52e733cccd2de5a0c9
KhoiRobert <pdkhoi20@apcs.fitus.edu.vn>
2022-12-23 19:53:55 +0700

    Khoi

--- agent.py
+++ agent.py
@@ -1,21 +1,29 @@
+import random
+
+
 class Agent():
     def __init__(self):
         self.__is_win = False
         self.__pos = self.__set_init_pos()
         self.__hint_list = []
 
-    def __set_init_pos(self):
+    def __set_init_pos(self, map):
         # Add code
-        return (0,0)
+        x = random.randint(0, map.size()-1)
+        y = random.randint(0, map.size()-1)
+        while (map[y][x][-1] == '0' or map[x][y][-1] == 'M' or map[y][x][-1] == 'P'):
+            x = random.randint(0, map.size()-1)
+            y = random.randint(0, map.size()-1)
+        return (x, y)
 
     def get_pos(self):
         return self.__pos
 
     def check_hint(hint):
-        # Add code 
+        # Add code
         return True
 
-    def is_win(self): # Use to check whether agent win
+    def is_win(self):  # Use to check whether agent win
         return self.__is_win
 
     def action(self, hint):
